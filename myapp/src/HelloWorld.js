53ea7494e4ecebff9ed4c4c442934d5d40e8073c
Xinhai Leng <103094152+OliverLengLLL@users.noreply.github.com>
2024-03-09 16:24:19 -0500

    Successfully Merge tailwind into React (#45)
    * starting install the tailwind
    
    * Successfully Merge tailwind into React

--- myapp/src/HelloWorld.js
+++ myapp/src/HelloWorld.js
@@ -16,7 +16,7 @@ function HelloWorld() {
 
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1 className='text-xl text-neutral-600'>Hello, World!</h1>
       <p>{message}</p>
     </div>
   );
