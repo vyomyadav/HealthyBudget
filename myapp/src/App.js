10371a895980ec2660acbf7ed2913a2871e52989
Vyom Yadav <vyomyadav20@gmail.com>
2024-04-02 20:01:26 -0400

    Login Page

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,54 +1,19 @@
-import { jwtDecode } from 'jwt-decode';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from "react"
 
-function App() {
+import { Route, Navigate, Routes } from "react-router-dom"
 
-  const [ user, setUser ] = useState({});
+import Routers from "./containers/routes"
 
-  function handleCallbackResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
-    var userObject = jwtDecode(response.credential);
-    console.log(userObject);
-    setUser(userObject);
-    document.getElementById("signInDiv").hidden = true;
-  }
 
-  function handleSignOut(event) {
-    setUser({});
-    document.getElementById("signInDiv").hidden = false;
-  }
-
-  useEffect(() => {
-    /* global google */
-    google.accounts.id.initialize({
-      client_id: "140911216337-hecompldoi9tnk36dc1utr07h86q0khg.apps.googleusercontent.com",
-      callback: handleCallbackResponse
-    });
-
-    google.accounts.id.renderButton(
-      document.getElementById("signInDiv"),
-      { theme: "outline", size: "large", text: "signIn" }
-    );
-  }, [])
-  //If we have no user: sign in button
-  //If we have a user: show log out button
-  return (
-    <div className='App'>
-      <div id='signInDiv'></div> 
-      {
-        Object.keys(user).length !== 0 && 
-        <button className ="sign-out-button" onClick={(e) => handleSignOut(e)}>Sign Out</button>
-      }
-      
-      { user && 
-        <div className='user-info'>
-          <img src={user.picture}></img>
-          <h3 className='user-name'>{user.name}</h3>
-        </div> 
-      }
+const App = () => (
+  <>
+    <div className="bg-background flex flex-col bg-cover min-h-screen bg-center">
+      <Routes>
+        <Route exact path="" element={<Navigate to="/login" />} />
+        <Route path="/*" element={<Routers />} />
+      </Routes>
     </div>
-  );
-}
+  </>
+)
 
-export default App;
+export default App
