73d10df8ddea02b384a01ab95d647639fae06b5f
Garima Mahajan <garima.kapilmhjn@gmail.com>
2024-04-10 18:26:41 -0400

    change directory from healhtybudget to myapi

--- HealthyBudget/urls.py
+++ HealthyBudget/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -27,5 +26,3 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- myapi/urls.py
+++ myapi/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path('hello-world/', views.hello_world, name='hello_world'),
@@ -8,3 +10,7 @@ urlpatterns = [
 	path('logout', views.UserLogout.as_view(), name='logout'),
 	path('user', views.UserView.as_view(), name='user'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
